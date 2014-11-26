80c1ed4b3256a109b3595f71a94b1af6afe02139
Akira SUENAMI <suenami@fact-real.com>
2014-11-26 19:19:17 +0900

    Localize feature files and production code in Japanese.

--- production/app/controllers/todos_controller.rb
+++ production/app/controllers/todos_controller.rb
@@ -28,7 +28,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to @todo, notice: I18n.t('todo.create', scope: 'messages') }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to @todo, notice:  I18n.t('todo.update', scope: 'messages') }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
